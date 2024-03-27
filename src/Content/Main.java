6f7b9bf5e31839cc67a9e46e9f135b32bab0c22a
Benson Nguyen <nihkvalentine@gmail.com>
2024-03-27 09:24:25 -0700

    First Commit

--- src/Content/Main.java
+++ src/Content/Main.java
@@ -5,5 +5,9 @@ public class Main {
 public static void main(String[] args) {
     DatingAppMenu menu = new DatingAppMenu();
     menu.setVisible(true);
-    }
+
+    String filepath = "src/Content/Swervin.wav";
+    musicStuff musicObject = new musicStuff();
+musicObject.playMusic(filepath);
+}
 }
